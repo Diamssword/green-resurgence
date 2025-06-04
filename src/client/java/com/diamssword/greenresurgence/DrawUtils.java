46b63c40e415a6b757411f257074b62d4e9332fe
Diamssword <diamssword@nope.me>
2025-06-04 15:48:51 +0200

    Test fading hotbar

--- src/client/java/com/diamssword/greenresurgence/DrawUtils.java
+++ src/client/java/com/diamssword/greenresurgence/DrawUtils.java
@@ -1,31 +1,56 @@
 package com.diamssword.greenresurgence;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import io.wispforest.owo.ui.core.OwoUIDrawContext;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import org.joml.Matrix4f;
 
 public class DrawUtils {
-	public static void drawTexture(DrawContext ctx,
-								   Identifier texture, int x, int y, int width, int height, float u, float v, float regionWidth, float regionHeight, int textureWidth, int textureHeight
-	) {
-		drawTexturedQuad(ctx, texture, x, x + width, y, y + height, 0, (u + 0.0F) / (float) textureWidth,
-				(u + regionWidth) / (float) textureWidth,
-				(v + 0.0F) / (float) textureHeight,
-				(v + regionHeight) / (float) textureHeight);
-	}
+    public static void drawTexture(DrawContext ctx,
+                                   Identifier texture, int x, int y, int width, int height, float u, float v, float regionWidth, float regionHeight, int textureWidth, int textureHeight
+    ) {
+        drawTexturedQuad(ctx, texture, x, x + width, y, y + height, 0, (u + 0.0F) / (float) textureWidth,
+                (u + regionWidth) / (float) textureWidth,
+                (v + 0.0F) / (float) textureHeight,
+                (v + regionHeight) / (float) textureHeight);
+    }
 
-	static void drawTexturedQuad(DrawContext ctx, Identifier texture, int x1, int x2, int y1, int y2, int z, float u1, float u2, float v1, float v2) {
-		RenderSystem.setShaderTexture(0, texture);
-		RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-		Matrix4f matrix4f = ctx.getMatrices().peek().getPositionMatrix();
-		BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-		bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-		bufferBuilder.vertex(matrix4f, (float) x1, (float) y1, (float) z).texture(u1, v1).next();
-		bufferBuilder.vertex(matrix4f, (float) x1, (float) y2, (float) z).texture(u1, v2).next();
-		bufferBuilder.vertex(matrix4f, (float) x2, (float) y2, (float) z).texture(u2, v2).next();
-		bufferBuilder.vertex(matrix4f, (float) x2, (float) y1, (float) z).texture(u2, v1).next();
-		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
-	}
+    static void drawTexturedQuad(DrawContext ctx, Identifier texture, int x1, int x2, int y1, int y2, int z, float u1, float u2, float v1, float v2) {
+        RenderSystem.setShaderTexture(0, texture);
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        Matrix4f matrix4f = ctx.getMatrices().peek().getPositionMatrix();
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+        bufferBuilder.vertex(matrix4f, (float) x1, (float) y1, (float) z).texture(u1, v1).next();
+        bufferBuilder.vertex(matrix4f, (float) x1, (float) y2, (float) z).texture(u1, v2).next();
+        bufferBuilder.vertex(matrix4f, (float) x2, (float) y2, (float) z).texture(u2, v2).next();
+        bufferBuilder.vertex(matrix4f, (float) x2, (float) y1, (float) z).texture(u2, v1).next();
+        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+    }
+
+
+    public static void renderHotbarItem(MinecraftClient client, OwoUIDrawContext context, int x, int y, float delta, PlayerEntity player, ItemStack stack, int seed) {
+        if (!stack.isEmpty()) {
+            float g = (float) stack.getBobbingAnimationTime() - delta;
+            if (g > 0.0F) {
+                float h = 1.0F + g / 5.0F;
+                context.getMatrices().push();
+                context.getMatrices().translate((float) (x + 8), (float) (y + 12), 0.0F);
+                context.getMatrices().scale(1.0F / h, (h + 1.0F) / 2.0F, 1.0F);
+                context.getMatrices().translate((float) (-(x + 8)), (float) (-(y + 12)), 0.0F);
+            }
+
+            context.drawItem(player, stack, x, y, seed);
+            if (g > 0.0F) {
+                context.getMatrices().pop();
+            }
+
+            context.drawItemInSlot(client.textRenderer, stack, x, y);
+        }
+    }
 }
--- src/client/java/com/diamssword/greenresurgence/gui/components/hud/SingleSlotComponent.java
+++ src/client/java/com/diamssword/greenresurgence/gui/components/hud/SingleSlotComponent.java
@@ -1,5 +1,6 @@
 package com.diamssword.greenresurgence.gui.components.hud;
 
+import com.diamssword.greenresurgence.DrawUtils;
 import com.diamssword.greenresurgence.GreenResurgence;
 import com.mojang.blaze3d.systems.RenderSystem;
 import io.wispforest.owo.ui.base.BaseComponent;
@@ -8,27 +9,22 @@ import io.wispforest.owo.ui.core.Sizing;
 import io.wispforest.owo.ui.parsing.UIModel;
 import io.wispforest.owo.ui.parsing.UIParsing;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.AttackIndicator;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Arm;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.collection.DefaultedList;
 import org.w3c.dom.Element;
 
-import java.util.List;
 import java.util.Map;
 
-public class SingleSlotComponent extends BaseComponent implements IHideableComponent{
+public class SingleSlotComponent extends BaseComponent implements IHideableComponent {
 
-    private final int textureSize=46;
-    private ItemStack stack=ItemStack.EMPTY;
+    private final int textureSize = 46;
+    private ItemStack stack = ItemStack.EMPTY;
     protected boolean blend = false;
     protected final Identifier texture;
 
-    protected boolean indicatorMode=false;
-    protected static final Identifier textureAtt= GreenResurgence.asRessource("textures/gui/hud/attack.png");
+    protected boolean indicatorMode = false;
+    protected static final Identifier textureAtt = GreenResurgence.asRessource("textures/gui/hud/attack.png");
     private boolean hidden;
 
     protected SingleSlotComponent(Identifier texture) {
@@ -40,6 +36,7 @@ public class SingleSlotComponent extends BaseComponent implements IHideableCompo
         this.blend = blend;
         return this;
     }
+
     @Override
     protected void applySizing() {
         final var horizontalSizing = this.horizontalSizing.get();
@@ -48,34 +45,40 @@ public class SingleSlotComponent extends BaseComponent implements IHideableCompo
         this.height = verticalSizing.inflate(this.space.height() - margins.vertical(), this::determineVerticalContentSize);
         this.width = horizontalSizing.inflate(this.space.width() - margins.horizontal(), this::determineHorizontalContentSize);
     }
+
     public boolean blend() {
         return this.blend;
     }
+
     @Override
     public void update(float delta, int mouseX, int mouseY) {
         super.update(delta, mouseX, mouseY);
 
     }
+
     @Override
     protected int determineHorizontalContentSize(Sizing sizing) {
         return 22;
     }
+
     @Override
     public void hidden(boolean hidden) {
-        this.hidden=hidden;
+        this.hidden = hidden;
     }
 
     @Override
     public boolean isHidden() {
         return this.hidden;
     }
+
     @Override
     protected int determineVerticalContentSize(Sizing sizing) {
         return 22;
     }
+
     @Override
     public void draw(OwoUIDrawContext context, int mouseX, int mouseY, float partialTicks, float delta) {
-        if(hidden)
+        if (hidden)
             return;
         RenderSystem.enableDepthTest();
 
@@ -86,8 +89,8 @@ public class SingleSlotComponent extends BaseComponent implements IHideableCompo
 
         var matrices = context.getMatrices();
 
-        var mc=MinecraftClient.getInstance();
-        if(!indicatorMode) {
+        var mc = MinecraftClient.getInstance();
+        if (!indicatorMode) {
             matrices.push();
             matrices.translate(x, y, 0);
             context.drawTexture(this.texture, 0, 0, 22, 22, 0, 22, 22, 22, textureSize, textureSize);
@@ -95,19 +98,17 @@ public class SingleSlotComponent extends BaseComponent implements IHideableCompo
             matrices.pop();
             matrices.push();
             matrices.translate(x, y, 0);
-            renderHotbarItem(mc, context, 3, 3, delta, mc.player, stack, 42);
+            DrawUtils.renderHotbarItem(mc, context, 3, 3, delta, mc.player, stack, 42);
             matrices.pop();
-        }
-        else
-        {
+        } else {
             matrices.push();
             matrices.translate(x, y, 0);
             if (mc.options.getAttackIndicator().getValue() == AttackIndicator.HOTBAR) {
                 float f = mc.player.getAttackCooldownProgress(0.0F);
                 if (f < 1.0F) {
-                    int p = (int)(f * 19.0F);
+                    int p = (int) (f * 19.0F);
                     context.drawTexture(textureAtt, 2, 2, 18, 18, 0, 0, 18, 18, 36, 18);
-                    context.drawTexture(textureAtt, 2, 2+(18-p), 18, p, 18, 18-p, 18, p, 36, 18);
+                    context.drawTexture(textureAtt, 2, 2 + (18 - p), 18, p, 18, 18 - p, 18, p, 36, 18);
 
                 }
             }
@@ -118,31 +119,13 @@ public class SingleSlotComponent extends BaseComponent implements IHideableCompo
         }
 
     }
-    public static void renderHotbarItem(MinecraftClient client,OwoUIDrawContext context, int x, int y, float f, PlayerEntity player, ItemStack stack, int seed) {
-        if (!stack.isEmpty()) {
-            float g = (float)stack.getBobbingAnimationTime() - f;
-            if (g > 0.0F) {
-                float h = 1.0F + g / 5.0F;
-                context.getMatrices().push();
-                context.getMatrices().translate((float)(x + 8), (float)(y + 12), 0.0F);
-                context.getMatrices().scale(1.0F / h, (h + 1.0F) / 2.0F, 1.0F);
-                context.getMatrices().translate((float)(-(x + 8)), (float)(-(y + 12)), 0.0F);
-            }
 
-            context.drawItem(player, stack, x, y, seed);
-            if (g > 0.0F) {
-                context.getMatrices().pop();
-            }
-
-            context.drawItemInSlot(client.textRenderer, stack, x, y);
-        }
-    }
     public ItemStack getStack() {
         return stack;
     }
 
     public void setStacks(ItemStack stack) {
-        this.stack=stack;
+        this.stack = stack;
     }
 
     public boolean isIndicatorMode() {
@@ -159,9 +142,10 @@ public class SingleSlotComponent extends BaseComponent implements IHideableCompo
         UIParsing.apply(children, "blend", UIParsing::parseBool, this::blend);
 
     }
+
     public static SingleSlotComponent parse(Element element) {
         UIParsing.expectAttributes(element, "texture");
         var textureId = UIParsing.parseIdentifier(element.getAttributeNode("texture"));
-        return new SingleSlotComponent(new Identifier(textureId.getNamespace(),"textures/gui/"+textureId.getPath()));
+        return new SingleSlotComponent(new Identifier(textureId.getNamespace(), "textures/gui/" + textureId.getPath()));
     }
 }
