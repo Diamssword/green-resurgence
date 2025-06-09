cc7e83057e05000d1ff065450b317540ae4a5a71
Diamssword <diamssword@nope.me>
2025-06-09 11:04:47 +0200

    Made inverted version of FreeRowGridLayout.java and added a subpath system for GenericBlocks modeles

--- src/client/java/com/diamssword/greenresurgence/gui/PlayerBasedGui.java
+++ src/client/java/com/diamssword/greenresurgence/gui/PlayerBasedGui.java
@@ -23,74 +23,75 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PlayerBasedGui<T extends MultiInvScreenHandler> extends MultiInvHandledScreen<T, FlowLayout> {
-	public final String subscreen;
-	private FreeRowGridLayout gridBonus;
-	private FreeRowGridLayout gridMalus;
-	private final Map<StatusEffect, EffectComponent> activeBonus = new HashMap<>();
-	private final Map<StatusEffect, EffectComponent> activeMalus = new HashMap<>();
+    public final String subscreen;
+    private FreeRowGridLayout gridBonus;
+    private FreeRowGridLayout gridMalus;
+    private final Map<StatusEffect, EffectComponent> activeBonus = new HashMap<>();
+    private final Map<StatusEffect, EffectComponent> activeMalus = new HashMap<>();
 
-	public PlayerBasedGui(T handler, String subscreen) {
-		super(handler, FlowLayout.class, BaseUIModelScreen.DataSource.asset(GreenResurgence.asRessource("survival/player_inventory")));
-		this.subscreen = subscreen;
-	}
+    public PlayerBasedGui(T handler, String subscreen) {
+        super(handler, FlowLayout.class, BaseUIModelScreen.DataSource.asset(GreenResurgence.asRessource("survival/player_inventory")));
+        this.subscreen = subscreen;
+    }
 
 
-	@Override
-	protected void build(FlowLayout rootComponent) {
-		var sub = rootComponent.childById(SubScreenLayout.class, "subcontainer");
-		if (sub != null) {
-			sub.setLayout(subscreen);
-			rootComponent.onChildMutated(sub);
-		}
-		gridBonus = rootComponent.childById(FreeRowGridLayout.class, "bonusLayout");
-		gridMalus = rootComponent.childById(FreeRowGridLayout.class, "malusLayout");
-		var playerComp = rootComponent.childById(PlayerComponent.class, "playerSkin");
-		var player = playerComp.entity();
-		var cp = new NbtCompound();
-		MinecraftClient.getInstance().player.getComponent(Components.PLAYER_DATA).writeToNbt(cp);
-		var dt = player.getComponent(Components.PLAYER_DATA);
-		dt.readFromNbt(cp);
-		var dt1 = player.getComponent(Components.PLAYER_INVENTORY);
-		dt1.setBackpackStack(MinecraftClient.getInstance().player.getComponent(Components.PLAYER_INVENTORY).getBackpackStack());
-		dt.appearance.refreshSkinData();
-	}
+    @Override
+    protected void build(FlowLayout rootComponent) {
+        var sub = rootComponent.childById(SubScreenLayout.class, "subcontainer");
+        if (sub != null) {
+            sub.setLayout(subscreen);
+            rootComponent.onChildMutated(sub);
+        }
+        gridBonus = rootComponent.childById(FreeRowGridLayout.class, "bonusLayout");
+        gridMalus = rootComponent.childById(FreeRowGridLayout.class, "malusLayout");
+        gridBonus.setInverted(true);
+        var playerComp = rootComponent.childById(PlayerComponent.class, "playerSkin");
+        var player = playerComp.entity();
+        var cp = new NbtCompound();
+        MinecraftClient.getInstance().player.getComponent(Components.PLAYER_DATA).writeToNbt(cp);
+        var dt = player.getComponent(Components.PLAYER_DATA);
+        dt.readFromNbt(cp);
+        var dt1 = player.getComponent(Components.PLAYER_INVENTORY);
+        dt1.setBackpackStack(MinecraftClient.getInstance().player.getComponent(Components.PLAYER_INVENTORY).getBackpackStack());
+        dt.appearance.refreshSkinData();
+    }
 
-	@Override
-	protected void handledScreenTick() {
-		super.handledScreenTick();
-		if (gridBonus != null && gridMalus != null) {
-			Collection<StatusEffectInstance> collection = this.client.player.getStatusEffects();
-			drawEffects(collection.stream().filter(v -> v.getEffectType().isBeneficial()).collect(Collectors.toList()), gridBonus, activeBonus);
-			drawEffects(collection.stream().filter(v -> !v.getEffectType().isBeneficial()).collect(Collectors.toList()), gridMalus, activeMalus);
+    @Override
+    protected void handledScreenTick() {
+        super.handledScreenTick();
+        if (gridBonus != null && gridMalus != null) {
+            Collection<StatusEffectInstance> collection = this.client.player.getStatusEffects();
+            drawEffects(collection.stream().filter(v -> v.getEffectType().isBeneficial()).collect(Collectors.toList()), gridBonus, activeBonus);
+            drawEffects(collection.stream().filter(v -> !v.getEffectType().isBeneficial()).collect(Collectors.toList()), gridMalus, activeMalus);
 
-		}
-	}
+        }
+    }
 
-	private void drawEffects(Collection<StatusEffectInstance> effects, FreeRowGridLayout grid, Map<StatusEffect, EffectComponent> tracker) {
-		var d1 = effects.stream().map(StatusEffectInstance::getEffectType).toList();
-		var it = tracker.entrySet().iterator();
-		while (it.hasNext()) {
-			var a = it.next();
-			if (!d1.contains(a.getKey())) {
-				a.getValue().remove();
-				it.remove();
-			}
-		}
-		for (var eff : effects) {
-			if (!tracker.containsKey(eff.getEffectType())) {
-				var d = new EffectComponent(eff);
-				d.sizing(Sizing.fixed(22));
-				d.margins(Insets.of(1));
-				tracker.put(eff.getEffectType(), d);
-				grid.child(d);
-			}
+    private void drawEffects(Collection<StatusEffectInstance> effects, FreeRowGridLayout grid, Map<StatusEffect, EffectComponent> tracker) {
+        var d1 = effects.stream().map(StatusEffectInstance::getEffectType).toList();
+        var it = tracker.entrySet().iterator();
+        while (it.hasNext()) {
+            var a = it.next();
+            if (!d1.contains(a.getKey())) {
+                a.getValue().remove();
+                it.remove();
+            }
+        }
+        for (var eff : effects) {
+            if (!tracker.containsKey(eff.getEffectType())) {
+                var d = new EffectComponent(eff);
+                d.sizing(Sizing.fixed(22));
+                d.margins(Insets.of(1));
+                tracker.put(eff.getEffectType(), d);
+                grid.child(d);
+            }
 
-			//	grid.removeChild();
-		}
-	}
+            //	grid.removeChild();
+        }
+    }
 
-	@Override
-	protected void drawBackground(DrawContext var1, float var2, int var3, int var4) {
+    @Override
+    protected void drawBackground(DrawContext var1, float var2, int var3, int var4) {
 
-	}
+    }
 }
--- src/client/java/com/diamssword/greenresurgence/gui/components/FreeRowGridLayout.java
+++ src/client/java/com/diamssword/greenresurgence/gui/components/FreeRowGridLayout.java
@@ -15,161 +15,175 @@ import java.util.*;
 
 public class FreeRowGridLayout extends BaseParentComponent {
 
-	protected final int columns;
+    protected final int columns;
+    private boolean inverted = false;
 
-	protected final List<Component> children = new ArrayList<>();
+    protected final List<Component> children = new ArrayList<>();
 
-	protected Size contentSize = Size.zero();
+    protected Size contentSize = Size.zero();
 
-	protected FreeRowGridLayout(Sizing horizontalSizing, Sizing verticalSizing, int columns) {
-		super(horizontalSizing, verticalSizing);
-		this.columns = columns;
-	}
+    protected FreeRowGridLayout(Sizing horizontalSizing, Sizing verticalSizing, int columns) {
+        super(horizontalSizing, verticalSizing);
+        this.columns = columns;
+    }
 
-	@Override
-	protected int determineHorizontalContentSize(Sizing sizing) {
-		return this.contentSize.width() + this.padding.get().right();
-	}
+    @Override
+    protected int determineHorizontalContentSize(Sizing sizing) {
+        return this.contentSize.width() + this.padding.get().right();
+    }
 
-	@Override
-	protected int determineVerticalContentSize(Sizing sizing) {
-		return this.contentSize.height() + this.padding.get().bottom();
-	}
+    @Override
+    protected int determineVerticalContentSize(Sizing sizing) {
+        return this.contentSize.height() + this.padding.get().bottom();
+    }
 
-	@Override
-	public void layout(Size space) {
-		int[] columnSizes = new int[this.columns];
-		int[] rowSizes = new int[rows()];
+    @Override
+    public void layout(Size space) {
+        int[] columnSizes = new int[this.columns];
+        int[] rowSizes = new int[rows()];
 
-		var childSpace = this.calculateChildSpace(space);
-		for (var child : this.children) {
-			if (child != null) {
-				child.inflate(childSpace);
-			}
-		}
+        var childSpace = this.calculateChildSpace(space);
+        for (var child : this.children) {
+            if (child != null) {
+                child.inflate(childSpace);
+            }
+        }
 
-		this.determineSizes(columnSizes, false);
-		this.determineSizes(rowSizes, true);
+        this.determineSizes(columnSizes, false);
+        this.determineSizes(rowSizes, true);
 
-		var mountingOffset = this.childMountingOffset();
-		var layoutX = new MutableInt(this.x + mountingOffset.width());
-		var layoutY = new MutableInt(this.y + mountingOffset.height());
+        var mountingOffset = this.childMountingOffset();
+        var layoutX = new MutableInt(this.x + mountingOffset.width());
+        var layoutY = new MutableInt(this.y + mountingOffset.height());
+        var layoutYInv = new MutableInt(this.y + mountingOffset.height());
 
-		for (int row = 0; row < rows(); row++) {
-			layoutX.setValue(this.x + mountingOffset.width());
+        if (inverted)
+            for (var dy : rowSizes)
+                layoutYInv.add(dy);
+        for (int row = 0; row < rows(); row++) {
+            layoutX.setValue(this.x + mountingOffset.width());
+            if (inverted)
+                layoutYInv.add(-rowSizes[row]);
+            for (int column = 0; column < this.columns; column++) {
+                int columnSize = columnSizes[column];
+                int rowSize = rowSizes[row];
 
-			for (int column = 0; column < this.columns; column++) {
-				int columnSize = columnSizes[column];
-				int rowSize = rowSizes[row];
+                this.mountChild(this.getChild((row * columns) + column), childSpace, child -> {
+                    var yC = inverted ? layoutYInv.intValue() + child.margins().get().top() + this.verticalAlignment().align(child.fullSize().height(), rowSize) :
+                            layoutY.intValue() + child.margins().get().bottom() + this.verticalAlignment().align(child.fullSize().height(), rowSize);
 
-				this.mountChild(this.getChild((row * columns) + column), childSpace, child -> {
-					child.mount(
-							this,
-							layoutX.intValue() + child.margins().get().left() + this.horizontalAlignment().align(child.fullSize().width(), columnSize),
-							layoutY.intValue() + child.margins().get().top() + this.verticalAlignment().align(child.fullSize().height(), rowSize)
-					);
-				});
+                    child.mount(
+                            this,
+                            layoutX.intValue() + child.margins().get().left() + this.horizontalAlignment().align(child.fullSize().width(), columnSize), yC
 
+                    );
+                });
 
-				layoutX.add(columnSizes[column]);
-			}
+                layoutX.add(columnSizes[column]);
+            }
+            layoutY.add(rowSizes[row]);
+        }
 
-			layoutY.add(rowSizes[row]);
-		}
+        this.contentSize = Size.of(layoutX.intValue() - this.x, layoutY.intValue() - this.y);
+    }
 
-		this.contentSize = Size.of(layoutX.intValue() - this.x, layoutY.intValue() - this.y);
-	}
+    public int rows() {
+        return (int) Math.ceil(this.children.size() / (float) columns);
+    }
 
-	public int rows() {
-		return (int) Math.ceil(this.children.size() / (float) columns);
-	}
+    @Override
+    public void draw(OwoUIDrawContext context, int mouseX, int mouseY, float partialTicks, float delta) {
+        super.draw(context, mouseX, mouseY, partialTicks, delta);
+        this.drawChildren(context, mouseX, mouseY, partialTicks, delta, children);
+    }
 
-	@Override
-	public void draw(OwoUIDrawContext context, int mouseX, int mouseY, float partialTicks, float delta) {
-		super.draw(context, mouseX, mouseY, partialTicks, delta);
-		this.drawChildren(context, mouseX, mouseY, partialTicks, delta, children);
-	}
+    protected void determineSizes(int[] sizes, boolean rows) {
+        if ((rows ? this.verticalSizing : this.horizontalSizing).get().method != Sizing.Method.CONTENT) {
+            Arrays.fill(sizes, (rows ? this.height - this.padding().get().vertical() : this.width - this.padding().get().horizontal()) / (rows ? this.rows() : this.columns));
+        } else {
+            for (int row = 0; row < rows(); row++) {
+                for (int column = 0; column < this.columns; column++) {
+                    var d = (row * this.columns) + column;
+                    if (d < this.children.size()) {
+                        final var child = this.children.get(d);
+                        if (child == null) continue;
 
-	protected void determineSizes(int[] sizes, boolean rows) {
-		if ((rows ? this.verticalSizing : this.horizontalSizing).get().method != Sizing.Method.CONTENT) {
-			Arrays.fill(sizes, (rows ? this.height - this.padding().get().vertical() : this.width - this.padding().get().horizontal()) / (rows ? this.rows() : this.columns));
-		} else {
-			for (int row = 0; row < rows(); row++) {
-				for (int column = 0; column < this.columns; column++) {
-					var d = (row * this.columns) + column;
-					if (d < this.children.size()) {
-						final var child = this.children.get(d);
-						if (child == null) continue;
+                        if (rows) {
+                            sizes[row] = Math.max(sizes[row], child.fullSize().height());
+                        } else {
+                            sizes[column] = Math.max(sizes[column], child.fullSize().width());
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-						if (rows) {
-							sizes[row] = Math.max(sizes[row], child.fullSize().height());
-						} else {
-							sizes[column] = Math.max(sizes[column], child.fullSize().width());
-						}
-					}
-				}
-			}
-		}
-	}
+    public Component getChild(int index) {
+        if (index < children.size())
+            return children.get(index);
+        return null;
+    }
 
-	public Component getChild(int index) {
-		if (index < children.size())
-			return children.get(index);
-		return null;
-	}
+    public FreeRowGridLayout child(Component child) {
+        if (child != null) {
+            this.children.add(child);
+            this.updateLayout();
+        }
+        return this;
+    }
 
-	public FreeRowGridLayout child(Component child) {
-		if (child != null) {
-			this.children.add(child);
-			this.updateLayout();
-		}
-		return this;
-	}
+    public FreeRowGridLayout setInverted(boolean inverted) {
+        this.inverted = inverted;
+        this.updateLayout();
 
-	public FreeRowGridLayout removeChild(int index) {
-		var currentChild = children.get(index);
-		if (currentChild != null) {
-			currentChild.dismount(DismountReason.REMOVED);
-			children.remove(index);
-			this.updateLayout();
-		}
+        return this;
+    }
 
-		return this;
-	}
+    public FreeRowGridLayout removeChild(int index) {
+        var currentChild = children.get(index);
+        if (currentChild != null) {
+            currentChild.dismount(DismountReason.REMOVED);
+            children.remove(index);
+            this.updateLayout();
+        }
 
-	@Override
-	public FreeRowGridLayout removeChild(Component child) {
-		for (int i = 0; i < this.children.size(); i++) {
-			if (Objects.equals(this.children.get(i), child)) {
-				this.removeChild(i);
-				break;
-			}
-		}
-		return this;
-	}
+        return this;
+    }
 
-	@Override
-	public List<Component> children() {
-		return this.children;
-	}
+    @Override
+    public FreeRowGridLayout removeChild(Component child) {
+        for (int i = 0; i < this.children.size(); i++) {
+            if (Objects.equals(this.children.get(i), child)) {
+                this.removeChild(i);
+                break;
+            }
+        }
+        return this;
+    }
 
-	@Override
-	public void parseProperties(UIModel model, Element element, Map<String, Element> children) {
-		super.parseProperties(model, element, children);
+    @Override
+    public List<Component> children() {
+        return this.children;
+    }
 
-		final var components = UIParsing
-				.get(children, "children", e -> UIParsing.<Element>allChildrenOfType(e, Node.ELEMENT_NODE))
-				.orElse(Collections.emptyList());
+    @Override
+    public void parseProperties(UIModel model, Element element, Map<String, Element> children) {
+        super.parseProperties(model, element, children);
 
-		for (var child : components) {
-			this.child(model.parseComponent(Component.class, child));
-		}
-	}
+        final var components = UIParsing
+                .get(children, "children", e -> UIParsing.<Element>allChildrenOfType(e, Node.ELEMENT_NODE))
+                .orElse(Collections.emptyList());
 
-	public static FreeRowGridLayout parse(Element element) {
-		UIParsing.expectAttributes(element, "columns");
-		int columns = UIParsing.parseUnsignedInt(element.getAttributeNode("columns"));
+        for (var child : components) {
+            this.child(model.parseComponent(Component.class, child));
+        }
+    }
 
-		return new FreeRowGridLayout(Sizing.content(), Sizing.content(), columns);
-	}
+    public static FreeRowGridLayout parse(Element element) {
+        UIParsing.expectAttributes(element, "columns");
+        int columns = UIParsing.parseUnsignedInt(element.getAttributeNode("columns"));
+
+        return new FreeRowGridLayout(Sizing.content(), Sizing.content(), columns);
+    }
 }
