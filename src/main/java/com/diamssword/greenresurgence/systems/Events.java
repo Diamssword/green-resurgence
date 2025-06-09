828eb2f1f6d11a162b8b0d04b1df861e2b7cd2fb
Diamssword <diamssword@nope.me>
2025-06-09 15:22:58 +0200

    Fixed size issues

--- src/main/java/com/diamssword/greenresurgence/systems/Events.java
+++ src/main/java/com/diamssword/greenresurgence/systems/Events.java
@@ -23,54 +23,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Events {
-	public static void init() {
-		List<PlayerEntity> scheluded = new ArrayList<>();
-		ServerPlayConnectionEvents.JOIN.register((h, s, serv) -> {
-			scheluded.add(h.player);
-		});
-		ServerTickEvents.END_SERVER_TICK.register(Lootables.loader::worldTick);
-		ServerTickEvents.END_SERVER_TICK.register(Recipes.loader::worldTick);
-		ServerTickEvents.END_SERVER_TICK.register(ClothingLoader.instance::worldTick);
-		ServerTickEvents.START_WORLD_TICK.register(w -> {
-			w.getEntitiesByType(TypeFilter.instanceOf(ItemEntity.class), i -> i.getStack().getItem() instanceof AbstractBackpackItem).forEach(v -> {
+    public static void init() {
+        List<PlayerEntity> scheluded = new ArrayList<>();
+        ServerPlayConnectionEvents.JOIN.register((h, s, serv) -> {
+            scheluded.add(h.player);
+        });
+        ServerTickEvents.END_SERVER_TICK.register(Lootables.loader::worldTick);
+        ServerTickEvents.END_SERVER_TICK.register(Recipes.loader::worldTick);
+        ServerTickEvents.END_SERVER_TICK.register(ClothingLoader.instance::worldTick);
+        ServerTickEvents.START_WORLD_TICK.register(w -> {
+            w.getEntitiesByType(TypeFilter.instanceOf(ItemEntity.class), i -> i.getStack().getItem() instanceof AbstractBackpackItem).forEach(v -> {
 
-				if (!((AbstractBackpackItem) v.getStack().getItem()).isInventoryEmpty(v.getStack())) {
-					w.spawnEntity(new BackpackEntity(w, v.getX(), v.getY(), v.getZ(), v.getStack()));
-					v.discard();
-				}
-			});
-		});
-		ServerTickEvents.END_SERVER_TICK.register((s) -> {
-			if (s.getOverworld().getTime() % 20 == 0) {
-				var ls = new ArrayList<>(scheluded);
+                if (!((AbstractBackpackItem) v.getStack().getItem()).isInventoryEmpty(v.getStack())) {
+                    w.spawnEntity(new BackpackEntity(w, v.getX(), v.getY(), v.getZ(), v.getStack()));
+                    v.discard();
+                }
+            });
+        });
+        ServerTickEvents.END_SERVER_TICK.register((s) -> {
+            if (s.getOverworld().getTime() % 20 == 0) {
+                var ls = new ArrayList<>(scheluded);
 
-				ls.forEach(l1 -> {//on laisse le temps a MC d'envoyer les tags et autres données avant
-					Channels.MAIN.serverHandle(l1).send(new DictionaryPackets.LootableList(Lootables.loader));
-					Channels.MAIN.serverHandle(l1).send(new DictionaryPackets.ClothingList(ClothingLoader.instance));
-					Channels.MAIN.serverHandle(l1).send(BaseInteractions.getPacket());
-					Channels.MAIN.serverHandle(l1).send(new DictionaryPackets.RecipeList(Recipes.loader));
-					CurrentZonePacket.sendDebugZone(l1.getWorld(), l1);
-					var g = l1.getWorld().getComponent(Components.BASE_LIST).getForPlayer(l1.getUuid(), false);
-					g.ifPresent(factionGuild -> Channels.MAIN.serverHandle(l1).send(new CurrentZonePacket.MyGuild(factionGuild.getId(), factionGuild.getName())));
+                ls.forEach(l1 -> {//on laisse le temps a MC d'envoyer les tags et autres données avant
+                    Channels.MAIN.serverHandle(l1).send(new DictionaryPackets.LootableList(Lootables.loader));
+                    Channels.MAIN.serverHandle(l1).send(new DictionaryPackets.ClothingList(ClothingLoader.instance));
+                    Channels.MAIN.serverHandle(l1).send(BaseInteractions.getPacket());
+                    Channels.MAIN.serverHandle(l1).send(new DictionaryPackets.RecipeList(Recipes.loader));
+                    CurrentZonePacket.sendDebugZone(l1.getWorld(), l1);
+                    var g = l1.getWorld().getComponent(Components.BASE_LIST).getForPlayer(l1.getUuid(), false);
+                    g.ifPresent(factionGuild -> Channels.MAIN.serverHandle(l1).send(new CurrentZonePacket.MyGuild(factionGuild.getId(), factionGuild.getName())));
+                    l1.calculateDimensions();
+                });
+                scheluded.clear();
+            }
 
-				});
-				scheluded.clear();
-			}
 
+        });
+        ServerTickEvents.START_SERVER_TICK.register((s) -> {
+            s.getPlayerManager().getPlayerList().forEach(pl -> {
+                PlayerTickEvent.onTick.invoker().onTick(pl, false);
+            });
+        });
+        ServerTickEvents.END_SERVER_TICK.register((s) -> {
+            s.getPlayerManager().getPlayerList().forEach(pl -> {
+                PlayerTickEvent.onTick.invoker().onTick(pl, true);
+            });
+        });
+        UseBlockCallback.EVENT.register(LootableLogic::onRightClick);
+        PlayerEvents.init();
 
-		});
-		ServerTickEvents.START_SERVER_TICK.register((s) -> {
-			s.getPlayerManager().getPlayerList().forEach(pl -> {
-				PlayerTickEvent.onTick.invoker().onTick(pl, false);
-			});
-		});
-		ServerTickEvents.END_SERVER_TICK.register((s) -> {
-			s.getPlayerManager().getPlayerList().forEach(pl -> {
-				PlayerTickEvent.onTick.invoker().onTick(pl, true);
-			});
-		});
-		UseBlockCallback.EVENT.register(LootableLogic::onRightClick);
-		PlayerEvents.init();
-
-	}
+    }
 }
